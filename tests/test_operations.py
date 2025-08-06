588e1f65d902472ad2621c96811223a8073269e4
Hameed Ul Haq <Hameed.ulHaq@MyNaghi.com>
2025-08-06 15:38:51 +0300

    unit test cases updated

---
+++ tests/test_operations.py
@@ -0,0 +1,11 @@
+from src.math_operations import add, sub
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(-1,1) == 0
+
+def test_sub():
+    assert sub(12,3) == 9
+    assert sub(1,1) == 0
+
+    
